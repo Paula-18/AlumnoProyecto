b63598e4387927b58790140dd7e6cb4fc84dc272
Paula-18 <paunahappy@gmail.com>
2019-08-19 12:40:05 -0700

    Agregar archivos de proyecto.

---
+++ AlumnoProyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlumnoProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matrícula: " + alumno1.Matricula);
+
+            Console.Read();
+        }
+    }
+}
